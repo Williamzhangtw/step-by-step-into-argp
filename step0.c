9cd684a63b6542a47736c1fdc9d0fa60ba96c80f
Williamzhangtw <izhangtengwei@gmail.com>
2022-10-27 16:09:53 +0800

    simple example

--- step0.c
+++ step0.c
@@ -19,8 +19,27 @@
 #include <stdio.h>
 #include <argp.h>
 
+#define ARG_KEY_D 'd'
+
+static int
+parse_opt (int key, char *arg, 
+	   struct argp_state *state)
+{
+  switch (key)
+    {
+    case ARG_KEY_D: printf (".\n"); break;
+    }
+  return 0;
+}
+
 int
 main (int argc, char **argv)
 {
-  return argp_parse (0, argc, argv, 0, 0, 0);
+  struct argp_option options[] = 
+    {
+	{ 0, ARG_KEY_D, 0, 0, "Show a dot on the screen"},
+	{ 0 }
+    };
+  struct argp argp = { options, parse_opt };
+  return argp_parse (&argp, argc, argv, 0, 0, 0);
 }
