2b80a1e4ecab4c528f17f17b2357e6fcd51b6548
Williamzhangtw <izhangtengwei@gmail.com>
2022-10-27 17:51:57 +0800

    OPTION_ALIAS example

--- src/main.c
+++ src/main.c
@@ -21,7 +21,9 @@
 #include <argp.h>
 #include <string.h>
 
+#define ARG_KEY_A 'a'
 #define ARG_KEY_D 'd'
+#define ARG_KEY_S 's'
 
 static int
 parse_opt (int key, char *arg, 
@@ -30,12 +32,21 @@ parse_opt (int key, char *arg,
   switch (key)
     {
     case ARG_KEY_D:
-        unsigned int i;
         if(arg == NULL){
             printf ("arg == NULL");
         }else{
-            for (i = 0; i < atoi (arg); i++){
+            for (int i = 0; i < atoi (arg); i++){
                 printf (".");
+            }
+        }
+        printf ("\n");
+        break;
+    case ARG_KEY_S:
+        if(arg == NULL){
+            printf ("smile arg == NULL");
+        }else{
+            for (int i = 0; i < atoi (arg); i++){
+                printf ("^^ ");
             }
         }
         printf ("\n");
@@ -50,7 +61,9 @@ main (int argc, char **argv)
   struct argp_option options[] = 
     {
 	{ "dot", ARG_KEY_D, "NUM", OPTION_ARG_OPTIONAL, "Show some dot on the screen"},
-	{ 0 }
+	{ "smile", ARG_KEY_S, "number", OPTION_ARG_OPTIONAL, "Show ^^ on the screen"},
+	{ "period", ARG_KEY_A, "FOO", OPTION_ALIAS, "Bar" },
+    { 0 }
     };
   struct argp argp = { options, parse_opt };
   return argp_parse (&argp, argc, argv, 0, 0, 0);
