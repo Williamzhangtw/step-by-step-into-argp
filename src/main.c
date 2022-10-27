42b30e147e7dc1bfc0b09bf2b70883fd506c20f2
Williamzhangtw <izhangtengwei@gmail.com>
2022-10-27 21:03:49 +0800

    ARGP_KEY_ARG and ARGP_KEY_END

--- src/main.c
+++ src/main.c
@@ -29,6 +29,7 @@ static int
 parse_opt (int key, char *arg, 
 	   struct argp_state *state)
 {
+    int *arg_count = state->input;
   switch (key)
     {
     case ARG_KEY_D:
@@ -53,6 +54,18 @@ parse_opt (int key, char *arg,
         break;
     case 777:
         return parse_opt ('d', "3", state);
+    case ARGP_KEY_ARG:
+        (*arg_count)--;
+        if (*arg_count >= 0)
+        printf (" %s", arg);
+        break;
+    case ARGP_KEY_END:
+        printf ("\n");
+        if (*arg_count >= 4)
+        argp_failure (state, 1, 0, "too few arguments");
+        else if (*arg_count < 0)
+        argp_failure (state, 1, 0, "too many arguments");
+        break;
     }
   return 0;
 }
@@ -60,6 +73,9 @@ parse_opt (int key, char *arg,
 int
 main (int argc, char **argv)
 {
+    for(int i=0; i<argc; i++){
+        // printf("arg[%d]: %s\n", i, argv[i]);
+    }
   struct argp_option options[] = 
     {
 	{ "dot", ARG_KEY_D, "NUM", OPTION_ARG_OPTIONAL, "Show some dot on the screen"},
@@ -68,6 +84,7 @@ main (int argc, char **argv)
     { "ellipsis", 777, 0, 0, "Show an ellipsis on the screen"},
     { 0 }
     };
-  struct argp argp = { options, parse_opt };
-  return argp_parse (&argp, argc, argv, 0, 0, 0);
+  int arg_count = 4;
+  struct argp argp = { options, parse_opt, "WORD [WORD [WORD [WORD]]]"};
+  return argp_parse (&argp, argc, argv, 0, 0, &arg_count);
 }
